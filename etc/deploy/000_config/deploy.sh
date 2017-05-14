1434af0db70ad25db37703c2ebcd896619d69cd1
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-05-14 02:46:46 +0000

    refs #1 user.name, user.email 設定を実装

--- etc/deploy/000_config/deploy.sh
+++ etc/deploy/000_config/deploy.sh
@@ -33,15 +33,20 @@ done
 # ~/.gitconfig が存在する場合は処理を終了する
 [[ -f ~/.gitconfig ]] && exit 0
 
+# [user] を設定する
+# name, email の値を read コマンドを使って設定する
+echo -n "user.name: "; read name
+echo -n "user.email: "; read email
+
 # [core] の editor を設定する
 editor=vi  # デフォルトは Vi とする
 is_installed vim && editor=vim  # Vim がインストールされている場合は Vim とする
 
 # 各種設定を sed で置換して ~/.gitconfig を作成する
 if [ ! -z ${dryrun} ]; then
-  sed -e "s/_EDITOR_/${editor}/" ${DOTGIT}/etc/deploy/000_config/gitconfig
+  sed -e "s/_NAME_/${name}/" -e "s/_EMAIL_/${email}/" -e "s/_EDITOR_/${editor}/" ${DOTGIT}/etc/deploy/000_config/gitconfig
 else
-  sed -e "s/_EDITOR_/${editor}/" ${DOTGIT}/etc/deploy/000_config/gitconfig > ~/.gitconfig
+  sed -e "s/_NAME_/${name}/" -e "s/_EMAIL_/${email}/" -e "s/_EDITOR_/${editor}/" ${DOTGIT}/etc/deploy/000_config/gitconfig > ~/.gitconfig
 fi
 
 exit 0
