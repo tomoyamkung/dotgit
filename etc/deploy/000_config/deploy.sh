347f75ba763c5454882e9afb748e4c4b3e5b6664
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-05-24 07:05:24 +0000

    refs #6 -f オプションを付与した場合でも ~/.gitconfig の "# dotgit: user specific settings" 以下にある設定は維持する仕組みを実装

--- etc/deploy/000_config/deploy.sh
+++ etc/deploy/000_config/deploy.sh
@@ -48,11 +48,27 @@ echo -n "user.email: "; read email
 editor=vi  # デフォルトは Vi とする
 is_installed vim && editor=vim  # Vim がインストールされている場合は Vim とする
 
+# ~/.gitconfig が存在しており、かつ、"# dotgit" から始まる行があればそこから下の行を一時的に保管しておく
+temp_file=/tmp/dotgit.gitconfig
+if [[ -f ~/.gitconfig ]] && [[ $(grep -q "# dotgit" ~/.gitconfig; echo $?) == 0 ]]; then
+  end_line=$(awk 'END{print NR}' ~/.gitconfig)
+  start_line=$(grep -n "# dotgit" ~/.gitconfig | awk -F: '{print $1}')
+  # 開始行と終了行が一致している場合は追加した設定がないので何もしない
+  if [[ $start_line != $end_line ]]; then
+    start_line=$(expr $start_line + 1)
+	sed -n "${start_line},${end_line}p" ~/.gitconfig > "$temp_file"
+  fi
+fi
+
 # 各種設定を sed で置換して ~/.gitconfig を作成する
 if [ ! -z ${dryrun} ]; then
   sed -e "s/_NAME_/${name}/" -e "s/_EMAIL_/${email}/" -e "s/_EDITOR_/${editor}/" ${DOTGIT}/etc/deploy/000_config/gitconfig
+  [[ -f "$temp_file" ]] && cat "$temp_file"
 else
   sed -e "s/_NAME_/${name}/" -e "s/_EMAIL_/${email}/" -e "s/_EDITOR_/${editor}/" ${DOTGIT}/etc/deploy/000_config/gitconfig > ~/.gitconfig
+  [[ -f "$temp_file" ]] && cat "$temp_file" >> ~/.gitconfig
 fi
+
+[[ -f "$temp_file" ]] && rm "$temp_file"
 
 exit 0
