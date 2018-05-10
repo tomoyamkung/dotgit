ca283d6594b8582fdfc95e12161790556faadf93
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2018-05-10 09:52:26 +0900

    refactoring

--- etc/deploy/010_prompt/bash/deploy.sh
+++ etc/deploy/010_prompt/bash/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eu
 
-# ライブラリスクリプトを読み込む
-. ${DOTGIT?"export DOTGIT=~/dotgit"}/bin/lib/dry_run.sh
-
 function usage() {
   cat <<EOF 1>&2
 Description:
@@ -11,43 +8,35 @@ Description:
   bash を対象とする。
 
 Usage:
-  $(basename ${0}) [-h] [-x]
+  $(basename ${0}) [-h]
 
 Options:
   -h print this
-  -x dry-run モードで実行する
 EOF
   exit 0
 }
 
-while getopts hx OPT
+while getopts h OPT
 do
   case "$OPT" in
     h) usage ;;
-    x) enable_dryrun ;;
     \?) usage ;;
   esac
 done
 
+prompt_script_path=~/.git-prompt.sh
 # ~/.bashrc に設定が存在する場合は処理を終了する
-grep -q "~/.git-prompt.sh" ~/.bashrc && exit 0
+grep -q "${prompt_script_path}" ~/.bashrc && exit 0
 
-# git-prompt.sh を取得するため /tmp に git を clone する
-if [[ ! -d /tmp/git ]]; then
-  ${dryrun} git clone https://github.com/git/git.git /tmp/git
-fi
-# clone したプロジェクトから git-prompt.sh を検索してホームディレクトリにコピーする
-if [[ ! -z ${dryrun} ]]; then
-  ${dryrun} 'find /tmp/git/ -type f -name "git-prompt.sh" | xargs -i echo cp ~/.git-prompt.sh'
-else
-  find /tmp/git -type f -name "git-prompt.sh" | xargs -i cp {} ~/.git-prompt.sh
+if [[ ! -f "${prompt_script_path}" ]]; then
+  # git-prompt.sh を取得するため /tmp に git を clone する
+  # clone したプロジェクトから git-prompt.sh を検索してホームディレクトリにコピーする
+  git clone https://github.com/git/git.git /tmp/git
+  find /tmp/git/ -type f -name "git-prompt.sh" | xargs -i cp {} "${prompt_script_path}"
+  rm -fr /tmp/git/
 fi
 
 # ~/.bashrc にプロンプトの設定を追加する
-if [[ ! -z ${dryrun} ]]; then
-  cat ${DOTGIT}/etc/deploy/010_prompt/bash/prompt
-else
-  cat ${DOTGIT}/etc/deploy/010_prompt/bash/prompt >> ~/.bashrc
-fi
+cat ${DOTGIT}/etc/deploy/010_prompt/bash/prompt >> ~/.bashrc
 
 exit 0
