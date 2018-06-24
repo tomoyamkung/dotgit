a2ee72123b51565b2e366de0be041a080234f441
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2018-06-24 14:29:46 +0000

    リファクタリング

--- etc/deploy/010_prompt/bash/deploy.sh
+++ etc/deploy/010_prompt/bash/deploy.sh
@@ -24,19 +24,28 @@ do
   esac
 done
 
-prompt_script_path=~/.git-prompt.sh
-# ~/.bashrc に設定が存在する場合は処理を終了する
-grep -q "${prompt_script_path}" ~/.bashrc && exit 0
-
-if [[ ! -f "${prompt_script_path}" ]]; then
-  # git-prompt.sh を取得するため /tmp に git を clone する
-  # clone したプロジェクトから git-prompt.sh を検索してホームディレクトリにコピーする
-  git clone https://github.com/git/git.git /tmp/git
-  find /tmp/git/ -type f -name "git-prompt.sh" | xargs -i cp {} "${prompt_script_path}"
-  rm -fr /tmp/git/
-fi
 
 # ~/.bashrc にプロンプトの設定を追加する
-cat ${DOTGIT}/etc/deploy/010_prompt/bash/prompt >> ~/.bashrc
+prompt_script_path=~/.git-prompt.sh
+if [[ ! -f "${prompt_script_path}" ]]; then
+  curl -o "${prompt_script_path}" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+  chmod 755 "${prompt_script_path}"
+fi
+if [[ ! `grep -q "${prompt_script_path}" ~/.bashrc` ]]; then
+  cat <<'EOF' >> ~/.bashrc
+[ -f ~/.git-prompt.sh ] && source ~/.git-prompt.sh
+PS1='[\u@\h \W`__git_ps1`]\$ '
+export PS1=${PS1}
+EOF
+fi
 
+# `git` コマンドの補完スクリプト設定を追加する
+completion_script_path=~/.git-completion.bash
+if [[ ! -f "${completion_script_path}" ]]; then
+  curl -o "${completion_script_path}" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+  chmod 755 "${ompletion_script_path}"
+fi
+if [[ ! `grep -q "${completion_script_path}" ~/.bashrc` ]]; then
+  echo "source ${completion_script_path}" >> ~/.bashrc
+fi
 exit 0
